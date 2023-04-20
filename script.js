1b1e6c4e0b12a8a323c60a57fc56e620ef41bb60
NickLu51o <purextrillest@gmail.com>
2023-04-19 23:19:00 -0700

    added grids and a function that can select for how many grids

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-let board = document.querySelector('.grid') 
+document.addEventListener ("DOMContentLoaded", function(){
+    createBoard(32)
+    console.log('hi')
+})
 
-board.style.gridTemplateColumns = 'repeat (16 , 1fr)'
-board.style.gridTemplateRows = 'repeat (16 , 1fr)'
+function createBoard(size) {
+    let board = document.querySelector(".board");
+    board.style.gridTemplateColumns = `repeat( ${size}, 1fr)`
+    board.style.gridTemplateRows = `repeat( ${size}, 1fr)`
+    
+    let numDivs = size * size
 
-for (let i=0; i<256; i++) {
-    let square = document.createElement('div')
-    square.style.backgroundColor = 'blue'
-    board.insertAdjacentElement('beforeend', square);
+    for(let i = 0 ; i< numDivs ; i++) {
+        let div = document.createElement("div");
+        div.style.backgroundColor="yellow"
+        board.insertAdjacentElement('beforeend', div);
+    }
 }
